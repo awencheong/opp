de65c0f89d438778959a0cfc48332b8952d09596
awencheong <awencheong@gmail.com>
2016-02-02 20:51:00 +0800

    add base namespace & help function

--- core/lib/Cmd.php
+++ core/lib/Cmd.php
@@ -13,6 +13,8 @@ class Cmd
         'data' => null
     );
 
+    private $op = 'EXEC';
+
     public $cmds = array();
 
     public $script;
@@ -27,30 +29,43 @@ class Cmd
         $this->_init();
     }
 
+    public function lastOperation()
+    {
+        return $this->op;
+    }
+
     private function _init()
     {
+        $this->op = 'EXEC';
         $this->script = $this->argv[0];
         $argv = array_slice($this->argv, 1);
         $cmd = '';
         foreach ($argv as $a) {
-            if (preg_match('/^--([^-^\s]+)/', $a, $match)) {
+            if (preg_match('/^--(.+)$/', $a, $match)) {
                 $cmd = $match[1];
+                $this->cmds[$cmd] = array();
                 continue;
             }
             
             if ($cmd != '') {
-                if ($tmp = \json_decode($a, true)) {
+                if ($tmp = json_decode($a, true)) {
                     $this->cmds[$cmd][] = $tmp;
                 } else {
                     $this->cmds[$cmd][] = $a;
                 }
             }
         }
+        
+        if (isset($this->cmds['HELP'])) {
+            $this->op = 'HELP';
+            unset($this->cmds['HELP']);
+        }
     }
 
     public function init(array $argv = null)
     {
         $this->argv = $argv;
+        $this->cmds = array();
         $this->_init();
     }
 
@@ -61,7 +76,17 @@ class Cmd
             $this->baseNameSpace = "";
         }
         foreach ($this->cmds as $modPath => $params) {
-            $this->_exec($modPath, $params);
+            if (strpos($modPath, "/") !== 0) {
+                $modPath = $this->baseNameSpace . "/" . trim($modPath, "/");
+            }
+            switch ($this->op) {
+                case 'HELP':
+                    $this->_helpFunc($modPath);
+                    break;
+                case 'EXEC':
+                    $this->_exec($modPath, $params);
+                    break;
+            }
         }
         return $this->return['data'];
     }
@@ -88,10 +113,42 @@ class Cmd
                 }
             }
         }
-        $res = call_user_func_array($this->_findFunc($this->baseNameSpace . "/" . trim($modPath, "/")), $params);
+        if (!$params && $this->return['started']) {
+            $params = array(
+                $this->return['data']
+            );
+        }
+        $callable = $this->_findFunc($modPath);
+        //$res = call_user_func_array($callable[0], $params);
+        if ($callable[0] instanceof \ReflectionMethod) {
+            $res = $callable[0]->invokeArgs($callable[1], $params);
+        } else {
+            $res = $callable[0]->invokeArgs($params);
+        }
         $this->return['started'] = true;
         $this->return['data'] = $res;
         return;
+    }
+
+    private function _helpFunc($modPath)
+    {
+        $res = array(); // name[=123]
+        try {
+            $refFunc = $this->_findFunc($modPath);
+            foreach ($refFunc[0]->getParameters() as $p) {
+                $desc = array();
+                if ($p->isDefaultValueAvailable()) {
+                    $desc['default'] = $p->getDefaultValue();
+                }
+                if ($p->isArray()) {
+                    $desc['isArray'] = true;
+                }
+                $res[$p->getName()] = $desc;
+            }
+        } catch (\Exception $e) {
+            $res = $e->getMessage();
+        }
+        $this->return['data'][$modPath] = $res;
     }
 
     private function _findFunc($modPath)
@@ -101,7 +158,9 @@ class Cmd
         //3. throw exception
         $modPath = str_replace("/", "\\", $modPath);
         if (function_exists($modPath)) {
-            return $modPath;
+            return array(
+                new \ReflectionFunction($modPath)
+            );
         }
         $split = strrpos($modPath, "\\");
         $class = substr($modPath, 0, $split);
@@ -112,13 +171,12 @@ class Cmd
         if (!class_exists($class)) {
             throw new \Exception("failed to find module;  function [$modPath]  or  method $class::$method not found");
         }
-        $obj = new $class();
-        if (!method_exists($obj, $method)) {
+        if (!method_exists($class, $method)) {
             throw new \Exception("failed to find module;  function [$modPath]  or  method $class::$method not found");
         }
         return array(
-            $obj,
-            $method
+            new \ReflectionMethod($class, $method),
+            new $class()
         );
     }
 }
