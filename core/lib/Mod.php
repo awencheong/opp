adcef85bc777c2eb4829ed4e6c894d6d6e711955
awencheong <awencheong@gmail.com>
2016-02-15 19:49:14 +0800

    add class Mod

--- core/lib/Mod.php
+++ core/lib/Mod.php
@@ -1,86 +1,115 @@
 <?php
-namespace	app;
+namespace app\lib;
 
-class	Mod
+class Mod
 {
-	private $moduleRoot = '';
-	public function setModuleRoot($namespace)
-	{
-		$this->moduleRoot = "\\" . trim($namespace, "\\");
-	}
 
-	/*
-	 * @param	$module,  
-	 * 		1)  user\login\get	
-	 * 		2)  user\login\get > \app\json
-	 *
-	 * @return	throw Exception on error;  or return result on success;
-	 */
-	public function call($modules, $params = array())
-	{
-		$modules = explode(">", $modules);
-		$res = null;
-		$start = true;
-		foreach ($modules as $m) {
-			if ($start) {
-				$res = $this->call_mod(trim($m), $params, 0);
-				$start = false;
-			} else {
-				$res = $this->call_mod(trim($m), $res, 1);
-			}
-		}
-		return $res;
-	}
+    private $started = false;
 
-	private function call_mod($module, $input = array(), $pipe = null)
-	{
-		$func = $this->createMethod($module, $params, $type, $class);
-		if ($pipe) {
-			$args = array($input);
-		} else {
-			$args = array();
-			foreach ($params as $name) {
-				if (isset($input[$name])) {
-					$args[$name] = $input[$name];
-				} else {
-					$args[$name] = null;
-				}
-			}
-		}
-		if ($type == "call_method") {
-			return $func->invokeArgs(new $class, $args);
-		} else if ($type == "call_func") {
-			return $func->invokeArgs($args);
-		}
-	}
+    public $res;
 
-	private function createMethod($module, &$params, &$type, &$class)
-	{
-		$tmp = $module;
-		$spl = strrpos($module, "\\");
-		$method = substr($module, $spl + 1);
-		$class = substr($module, 0, $spl);
-		if (strpos($class, "\\") !== 0) {
-			$class = $this->moduleRoot . "\\" . $class;
-			$module = $this->moduleRoot . "\\" . $module;
-		}
+    /*
+     * @param $mods, array( 'modPath' => $params )
+     */
+    public function callSequence(array $mods, $map_param_names = true)
+    {
+        foreach ($mods as $path => $params) {
+            $this->call($path, $params, $map_param_names);
+        }
+        $this->started = false;
+        $res = $this->res;
+        $this->res = null;
+        return $res;
+    }
 
-		$type = "";
-		if (function_exists($module)) {
-			$func = new \ReflectionFunction($module);
-			$type = "call_func";
-		} else if ($class && class_exists($class) && method_exists($class, $method)) {
-			$func = new \ReflectionMethod($class, $method);
-			$type = "call_method";
-		} else {
-			throw new \Exception("wrong module[$tmp], no function[$module] or class[$class::$method()] found");
-		}
-		$params = array();
-		foreach ($func->getParameters() as $p) {
-			$params[] = $p->name;
-		}
-		return $func;
+    public function call($modPath, $params, $map_param_names = true)
+    {
+        if (!$params) {
+            $params = array();
+        }
+        foreach ($params as &$p) {
+            if ($p == '$1' && $this->started) {
+                $p = $this->res;
+                continue;
+            }
+            
+            if (is_string($p) && preg_match('/^@(.*)/', $p, $match)) {
+                $filename = $match[1];
+                if (file_exists($filename) && is_readable($filename)) {
+                    $str = file_get_contents($filename);
+                    if (preg_match('/\.json$/', $filename)) {
+                        $p = json_decode($str, true);
+                    } else {
+                        $p = $str;
+                    }
+                } else {
+                    $p = '';
+                }
+            }
+        }
+        if (!$params && $this->started) {
+            $params = array(
+                $this->res
+            );
+            $map_param_names = false;
+        }
+        $callable = $this->_findFunc($modPath);
+        if ($map_param_names) {
+            $tmp = $params;
+            $params = array();
+            if ($callable['type'] == 'func') {
+                //1. function
+                $ref = new \ReflectionFunction($callable['name']);
+            } else {
+                //2. class method
+                $ref = new \ReflectionMethod($callable['name']);
+            }
+            foreach ($ref->getParameters() as $pm) {
+                $pname = $pm->getName();
+                if (isset($tmp[$pname])) {
+                    $params[] = $tmp[$pname];
+                } else {
+                    $params[] = null;
+                }
+            }
+        }
+        $res = call_user_func_array($callable['callable'], $params);
+        $this->started = true;
+        $this->res = $res;
+        return $this->res;
+    }
 
-	}
-
+    private function _findFunc($modPath)
+    {
+        //1. find function
+        //2. find class::method
+        //3. throw exception
+        if (function_exists($modPath)) {
+            return array(
+                'callable' => $modPath,
+                'name' => $modPath,
+                'type' => 'func'
+            );
+        }
+        $split = strrpos($modPath, "\\");
+        $class = substr($modPath, 0, $split);
+        $method = substr($modPath, $split + 1);
+        if (!$method) {
+            throw new \Exception("failed to find module;  function [$modPath]  or  method $class::$method not found");
+        }
+        if (!class_exists($class)) {
+            throw new \Exception("failed to find module;  function [$modPath]  or  method $class::$method not found");
+        }
+        if (!method_exists($class, $method)) {
+            throw new \Exception("failed to find module;  function [$modPath]  or  method $class::$method not found");
+        }
+        return array(
+            'callable' => array(
+                new $class(),
+                $method
+            ),
+            'name' => $class . "::" . $method,
+            'type' => 'method'
+        );
+    }
 }
