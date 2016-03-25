4ff3fdea2e8c9e0453f2f958192abeb124e6bc25
awencheong <awencheong@gmail.com>
2016-03-25 15:15:32 +0800

    Create exec.php

---
+++ core/exec.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: awencheong
+ * Date: 2015/9/1
+ * Time: 10:58
+ */
+
+/*
+ * ===============================================
+ * require >PHP5 see http://php.net/manual/en/class.reflectionmethod.php
+ * ===============================================
+ */
+namespace opp\core;
+
+include __DIR__ . "/include.php";
+
+use opp\core\lib\Cmd;
+global $argv;
+try {
+    $cmd = new Cmd($argv);
+    if (!$cmd->cmds) {
+        die("usage: php " . $cmd->script . " --cmd1 param1 ...  --cmd2 param1 ... \n\n");
+    }
+    print_r($cmd->exec("myapp/modules"));
+} catch (\Exception $e) {
+    echo ($e->getMessage() . "\n");
+}
+echo "\n";
